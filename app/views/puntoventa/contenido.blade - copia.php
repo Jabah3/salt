3f3a05f3107fa3aba3eee408fab62154d36ecf7c
javier <javah3@hotmail.com>
2015-01-16 22:11:41 -0600

    punto de venta
    punto

--- app/views/puntoventa/contenido.blade - copia.php
+++ app/views/puntoventa/contenido.blade - copia.php
@@ -1,13 +1,83 @@
 <?php $uri = Request::path(); ?> 
 
+<!-- CArrusell Loop -->
+<link rel="stylesheet" href="js/plugin/swiper-master/dist/idangerous.swiper.css">
+
+
+
+
+
+  <style>
+.swiper-container {
+  width: 220px;
+  height: 220px;
+  color: #fff;
+  text-align: center;
+}
+.red-slide {
+  background: #ca4040;
+}
+.blue-slide {
+  background: #4390ee;
+}
+.orange-slide {
+  background: #ff8604;
+}
+.green-slide {
+  background: #49a430;
+}
+.pink-slide {
+  background: #973e76;
+}
+
+.swiper-slide .title {
+  font-style: italic;
+  /*font-size: 42px;*/
+  /*margin-top: 80px;*/
+  margin-bottom: 0;
+  line-height: 45px;
+}
+
+.swiper-slide p {
+  font-style: italic;
+  font-size: 25px;
+}
+.pagination {
+  position: absolute;
+  z-index: 20;
+  left: 10px;
+  top: 10px;
+}
+.swiper-pagination-switch {
+  display: block;
+  width: 8px;
+  height: 8px;
+  border-radius: 8px;
+  background: #555;
+  margin: 0 0px 5px;
+  opacity: 0.8;
+  border: 1px solid #fff;
+  cursor: pointer;
+}
+.swiper-active-switch {
+  background: #fff;
+}
+
+
+
+
+.fila-base{ display: none; } /* fila base oculta */
+  </style>
+
+
+
+
 <div class="large-8 columns"> 
   Codigo del Producto: <input type="search" name="codigo" id="codigo" placeholder="Introduzca el codigo" autofocus/>
 </div>
 <br/>
+
 <div id="display"></div>
-
-
-
 
 
 
@@ -31,7 +101,7 @@
 
 
 
-<div class="large-5 columns"> 
+<div class="large-8 columns"> 
 
   <table id="tabla_productos">
 
@@ -49,6 +119,8 @@
       </tr>
     </thead>
 
+
+
     <tbody id="tabla_body_productos">
       <!--
       <tr id="prod_1">
@@ -60,6 +132,8 @@
           <td> <input type="submit" value="Eliminar" onclick="elimina_pod(1);" /> </td> 
       </tr-->
     </tbody>
+
+
 
   </table>
 
@@ -73,8 +147,35 @@
 
 
 
-<div class="large-7 columns"> 
-  @include('puntoventa.slider')
+
+
+
+
+<div class="large-4 columns"> 
+
+
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+
+
+  @if($productos)   
+    @foreach($productos as $producto)
+      <div class="swiper-slide" style="background:{{$producto->color}};">
+
+          <img {{--width="400" height="307"--}} ondblclick="myFunction({{$producto->id}},'{{$producto->nombre}}','{{$producto->precio_venta}}')" src="{{ 'imagenes/'.$producto->imagen }}" title="{{$producto->nombre}}" alt="{{$producto->nombre}}" />  
+          <div class="title">{{$producto->nombre}}</div>     
+          
+      </div>
+    @endforeach
+  @endif
+
+
+    </div>
+    <div class="pagination"></div>
+  </div>
+
+
+
 </div>
 
 
@@ -82,11 +183,33 @@
 
 
 
+
+
+
+<div class="large-6 columns"> 
+sd
+</div>
+
+
+
+
+<div class="large-6 columns"> 
+sd
+</div>
+
+
+
 <section id="resultados"></section>
 
 
 
+
+
+<script src="js/plugin/swiper-master/jquery-1.10.1.min.js"></script>
+<script src="js/plugin/swiper-master/dist/idangerous.swiper.min.js"></script>
+
 <script>
+
 function myFunction(id,nombre,precio){
     var rowCount = $('#tabla_body_productos tr').length;
 
@@ -131,4 +254,17 @@ $("#codigo").keypress(function(e) {
       });
 
 });
+
+
 </script>
+
+
+  <script>
+  var mySwiper = new Swiper('.swiper-container',{
+    pagination: '.pagination',
+    paginationClickable: true,
+    mode: 'vertical',
+    //slidesPerView: 2,
+    loop: true
+  })
+  </script>
